d04022a931a2b1ff226b84b22282e8789489dae6
Vlad Mitlosh <vlad.mitlosh@gmail.com>
2026-02-23 18:15:20 +0200

    solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,29 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  test('should return true for an empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  test('should return true for a valid isogram', () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+    expect(isIsogram('machine')).toBe(true);
+  });
+
+  test('should return false if letters repeat', () => {
+    expect(isIsogram('look')).toBe(false);
+    expect(isIsogram('eleven')).toBe(false);
+  });
+
+  test('should be case-insensitive', () => {
+    expect(isIsogram('Adam')).toBe(false);
+    expect(isIsogram('Moose')).toBe(false);
+    expect(isIsogram('Alphabet')).toBe(false);
+  });
+
+  test('should return true for single character', () => {
+    expect(isIsogram('a')).toBe(true);
+    expect(isIsogram('Z')).toBe(true);
+  });
 });
