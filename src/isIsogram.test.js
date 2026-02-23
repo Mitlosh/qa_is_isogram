ad42648c6f7fb3976755088dd466be66964a50b9
Vlad Mitlosh <vlad.mitlosh@gmail.com>
2026-02-23 19:37:02 +0200

    solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -1,34 +1,35 @@
-'use strict';
+"use strict";
 
-describe('isIsogram', () => {
-  const { isIsogram } = require('./isIsogram');
+describe("isIsogram", () => {
+  const { isIsogram } = require("./isIsogram");
 
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
 
-  test('should return true for an empty string', () => {
-    expect(isIsogram('')).toBe(true);
+  test("should return true for an empty string", () => {
+    expect(isIsogram("")).toBe(true);
   });
 
-  test('should return true for a valid isogram', () => {
-    expect(isIsogram('playgrounds')).toBe(true);
-    expect(isIsogram('machine')).toBe(true);
+  test("should return true for a valid isogram", () => {
+    expect(isIsogram("playgrounds")).toBe(true);
+    expect(isIsogram("machine")).toBe(true);
   });
 
-  test('should return false if letters repeat', () => {
-    expect(isIsogram('look')).toBe(false);
-    expect(isIsogram('eleven')).toBe(false);
+  test("should return false if letters repeat", () => {
+    expect(isIsogram("look")).toBe(false);
+    expect(isIsogram("eleven")).toBe(false);
   });
 
-  test('should be case-insensitive', () => {
-    expect(isIsogram('Adam')).toBe(false);
-    expect(isIsogram('Moose')).toBe(false);
-    expect(isIsogram('Alphabet')).toBe(false);
+  test("should be case-insensitive", () => {
+    expect(isIsogram("Adam")).toBe(false);
+    expect(isIsogram("Moose")).toBe(false);
+    expect(isIsogram("Alphabet")).toBe(false);
+    expect(isIsogram("Oops")).toBe(false);
   });
 
-  test('should return true for single character', () => {
-    expect(isIsogram('a')).toBe(true);
-    expect(isIsogram('Z')).toBe(true);
+  test("should return true for single character", () => {
+    expect(isIsogram("a")).toBe(true);
+    expect(isIsogram("Z")).toBe(true);
   });
 });
